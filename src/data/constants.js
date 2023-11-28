d06253770cfafaec75a66175d17d93ed47115dd2
vusumuzi <vgwala149@gmail.com>
2023-11-28 07:57:17 +0200

    confess

--- src/data/constants.js
+++ src/data/constants.js
@@ -258,7 +258,7 @@ export const projects = [
     description:
       "Designed and developed the Vexa project, a project management app that helps users and teams stay organized and on track. Implemented key features such as task tracking, team collaboration, and time tracking to improve productivity and project outcomes and also invite team/project members. The plan is to add a community of users where they can find developers and other team members and chat system implementation.",
     image:
-      "https://user-images.githubusercontent.com/64485885/234916413-96296f13-fe4b-4cc4-b215-e72bd7c27928.png",
+      "./images/confess.png",
     tags: [
       "Ionic",
       "Angular",
