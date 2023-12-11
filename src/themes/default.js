d2a4cb20cbe6629a76c92c7e647ff957a00e9ff3
vusumuzi <vgwala149@gmail.com>
2023-12-11 17:32:56 +0200

    dd

--- src/themes/default.js
+++ src/themes/default.js
@@ -19,11 +19,13 @@ const defaultTheme = {
   },
   // Breakpoints for responsive design
   breakpoints: {
+    xs: 'screen and (max-width: 280px)',
     sm: 'screen and (max-width: 640px)',
     md: 'screen and (max-width: 768px)',
     lg: 'screen and (max-width: 1024px)',
     xl: 'screen and (max-width: 1280px)'
   },
+  
 };
 
 export default defaultTheme;
